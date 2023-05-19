5be499e15ef9fa0fd27a8d776294da7f1b25c3f7
nur hikmah <nur.hikmah190504@gmail.com>
2023-05-19 12:18:45 +0700

    void addNode() {  int nim;  string nama;  Node* nodeBaru = new Node();  cout << "Masukkan NIM: ";  cin >> nim;  cout << "Masukan Nama: ";  cin >> nama;  nodeBaru->noMhs = nim;  nodeBaru->name = nama;

--- singlylinklist/singlylinklist.cpp
+++ singlylinklist/singlylinklist.cpp
@@ -8,3 +8,68 @@ struct Node {
 };
 
 Node* START = NULL;
+
+void addNode() {
+	int nim;
+	string nama;
+	Node* nodeBaru = new Node();
+	cout << "Masukkan NIM: ";
+	cin >> nim;
+	cout << "Masukan Nama: ";
+	cin >> nama;
+	nodeBaru->noMhs = nim;
+	nodeBaru->name = nama;
+
+	if (START == NULL || nim <= START->noMhs) {
+		if (START != NULL && nim == START->noMhs)
+		{
+			cout << "NIM sudah ada" << endl;
+			return;
+		}
+
+		nodeBaru->next = START;
+		START = nodeBaru;
+		return;
+	}
+
+	Node* previous = START;
+	Node* curret = START;
+
+	while ((curret != NULL) && (nim >= curret->noMhs))
+	{
+		if (nim == curret->noMhs)
+		{
+			cout << "NIM sudah ada" << endl;
+			return;
+		}
+		previous = curret;
+		curret = curret->next;
+	}
+
+	nodeBaru->next = curret;
+	previous->next = nodeBaru;
+}
+
+bool serachNode(int nim, Node* curret, Node* previous) {
+	previous = START;
+	curret = START;
+	while (curret != NULL && nim > curret->noMhs)
+	{
+		previous = curret;
+		curret = curret->next;
+	}
+
+	if (curret == NULL)
+	{
+		return false;
+	}
+	else if (curret->noMhs == nim)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+bool delete
